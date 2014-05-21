7f72fa1800c286231ab1748794137d126f5fdbff
pigram86 <todd@toddpigram.com>
2014-05-21 12:58:09 -0400

    initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,22 @@
+#
+# Cookbook Name:: java7
+# Recipe:: default
+#
+# Copyright (C) 2014 Todd Pigram
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+default['url']['x64'] = "https://dl.dropboxusercontent.com/u/36379525/jre-7u45-windows-x64.exe"
+default['url']['x86'] = "https://dl.dropboxusercontent.com/u/36379525/jre-7u45-windows-i586.exe"
+default['java64']['policy'] = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\JavaSoft\\Java Update\\Policy"
+default['java64']['file'] = "C:\\Program Files\\Java\\jre7\\bin\\java.exe"
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'java7'
+maintainer       'Todd Pigram'
+maintainer_email 'todd@toddpigram.com'
+license          'Apache 2.0'
+description      'Installs/Configures java7'
+long_description 'Installs/Configures java7'
+version          '0.2.0'
+depends          "windows"
+
